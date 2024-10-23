b692860a95d8480bf32e3cffc82be29aebbdda52
katriosa <ee.chistyakova@gmail.com>
2024-10-23 12:50:00 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>Заметки</h1>
+    <div class="new-note-controls">
+      <div class="note-color-picker">
+        <div class="color-option option-1"></div>
+        <div class="color-option option-2"></div>
+        <div class="color-option option-3"></div>
+      </div>
+      <!-- <button>Создать</button> -->
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.new-note-controls {
+  background-color: var(--color-board-background);
+  display: flex;
+  border-radius: 10px;
+  padding: 1rem;
+  gap: 1rem;
+}
+
+.note-color-picker {
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+}
+
+.color-option {
+  width: 2rem;
+  height: 2rem;
+  background-color: var(--color-note-bg-1);
+}
+
+.option-1 {
+  background-color: var(--color-note-bg-1);
+}
+
+.option-2 {
+  background-color: var(--color-note-bg-2);
+}
+
+.option-3 {
+  background-color: var(--color-note-bg-3);
+}
+
+/* button {
+  border: none;
+  background-color: var(--color-background);
+  color: var(--color-note-text);
+  cursor: pointer;
+  padding: 0 1rem;
+} */
+</style>
---
+++ src/components/Board.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="board"></div>
+</template>
+
+<style scoped>
+.board {
+  background-color: var(--color-board-background);
+  flex-grow: 1;
+}
+</style>
